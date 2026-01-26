7af05cfa5cdc7d61c94df57bdfd961ae0c97276c
Commanderk3 <diwangshukakoty@gmail.com>
2026-01-27 00:50:36 +0530

    feat: add ThemeContext for managing light and dark mode themes

---
+++ frontend/src/contexts/ThemeContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const ThemeContext = createContext();
+
+const lightMode = "lofi";
+const darkMode = "sunset";
+
+export const ThemeProvider = ({ children }) => {
+  const [isDark, setIsDark] = useState(
+    localStorage.getItem("theme") === lightMode
+  );
+
+  if (isDark === undefined) {
+    setIsDark(lightMode);
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute(
+      "data-theme",
+      isDark ? darkMode : lightMode
+    );
+    localStorage.setItem("theme", isDark ? darkMode : lightMode);
+  }, [isDark]);
+
+  return (
+    <ThemeContext.Provider value={{ isDark, setIsDark }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
